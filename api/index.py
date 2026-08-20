dbe1869d8ef347bc1576f46b56bc2ac5781d57c7
Iliyan Georgiev <georgiev.iliyan09@gmail.com>
2026-08-20 16:21:29 +0300

    Revert "fix: city keys"
    This reverts commit 476a56677b60af0826359e5360350f2783ef2014.

--- api/index.py
+++ api/index.py
@@ -26,10 +26,10 @@ with open(MODEL_PATHS["northpearl"], "rb") as f:
     northpearl_bundle = pickle.load(f)
 
 CITY_MODELS = {
-    "Варна": ("NorthPearl", northpearl_bundle),
-    "София": ("Atlas", atlas_bundle),
-    "Пловдив": ("Atlas", atlas_bundle),
-    "Бургас": ("Atlas", atlas_bundle),
+    "varna": ("NorthPearl", northpearl_bundle),
+    "sofia": ("Atlas", atlas_bundle),
+    "plovdiv": ("Atlas", atlas_bundle),
+    "burgas": ("Atlas", atlas_bundle),
 }
 
 
